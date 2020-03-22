31e5b37985121027c95d7225ddd09536d35445fa
Shuhan Hu <1119han@gmail.com>
2020-03-22 15:20:57 +0800

    Remove shan

--- shan/shan_page/src/components/logo.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-export default () => (
-  <div className="logo">
-    <span className="logo__jedi">Jedi</span>
-    <span className="logo__tours">Tours</span>
-  </div>
-)
